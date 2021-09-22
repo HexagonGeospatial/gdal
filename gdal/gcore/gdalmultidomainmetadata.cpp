ae341959adbf2556a0141de682b3711e8bf0e301
SAYERS Caleb <caleb.sayers@hexagon.com>
2021-09-22 14:22:07 +0800

    Reverted previous changes to gdalmultidomainmetadata

--- gdal/gcore/gdalmultidomainmetadata.cpp
+++ gdal/gcore/gdalmultidomainmetadata.cpp
@@ -72,7 +72,7 @@ void GDALMultiDomainMetadata::Clear()
     CSLDestroy( papszDomainList );
     papszDomainList = nullptr;
 
-    for( int i = 0; i <= nDomainCount; i++ )
+    for( int i = 0; i < nDomainCount; i++ )
     {
         delete papoMetadataLists[i];
     }
