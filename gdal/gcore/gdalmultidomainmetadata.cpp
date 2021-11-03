351434f18b15c11395c07dfdc8354f6679166b76
SAYERS Caleb <caleb.sayers@hexagon.com>
2021-11-03 13:12:30 +0800

    Set array index to nulptr to see if ASAN build is incorrectly picking up a leak

--- gdal/gcore/gdalmultidomainmetadata.cpp
+++ gdal/gcore/gdalmultidomainmetadata.cpp
@@ -75,6 +75,7 @@ void GDALMultiDomainMetadata::Clear()
     for( int i = 0; i < nDomainCount; i++ )
     {
         delete papoMetadataLists[i];
+        papoMetadataLists = nullptr;
     }
 
     CPLFree( papoMetadataLists );
